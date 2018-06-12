8f25db3231562403487c03c2232d57419f400e6d
xueyuhuan <501946152@qq.com>
2018-06-12 18:49:34 +0800

    服务中心end 添加注释

--- src/views/ServiceCenter.vue
+++ src/views/ServiceCenter.vue
@@ -16,18 +16,18 @@
         <ul class="menu" v-show="show.isObject">
             <li v-for="item in object" @click="search('object',item)">{{item.groupName}}</li>
         </ul>
-        <div class="conditions">检索条件：
+        <div class="conditions" v-show="isCondition">检索条件：
             <span v-for="item in condition">{{item}}</span>
-            <span><i class="fa fa-remove"></i>清空检索条件</span>
+            <span @click="clearCondition"><i class="fa fa-remove"></i>清空检索条件</span>
         </div>
         <ul class="list">
-            <li>
-                <img src="../assets/images/temp.png" alt="图片"/>
+            <li v-for="item in searchList"><a>
+                <img src="../assets/images/temp.png" alt="图片.jpg"/>
                 <div class="info">
-                    <header>工资查询</header>
-                    <p>服务类别:信息查询<i class="fa fa-eye"></i>&nbsp;987<i class="fa fa-star"></i>&nbsp;543</p>
+                    <header>{{item.name}}</header>
+                    <p>服务类别:{{item.type2Name}}<i class="fa fa-eye"></i>&nbsp;{{item.viewCount}}<i class="fa fa-star"></i>&nbsp;{{item.favCount}}</p>
                 </div>
-            </li>
+            </a></li>
         </ul>
     </div>
 </template>
@@ -43,13 +43,14 @@
           isMode:false,
           isObject:false
         },
+        isCondition:false,
         dropdown:['服务领域','负责部门','服务方式','服务对象'],
         field:[],//领域
         department:[],//部门
         mode:[{name:"线上服务",id:"1"}, {name:"线下服务",id:"0"},],//方式
         object:[],//对象
-        condition:['','','',''],
-        searchData:{
+        condition:[],//检索条件
+        searchData:{//请求列表数据格式
           page:1,
           limit:20,
           key:"",
@@ -90,13 +91,13 @@
           });
     },
     methods:{
-      initShow(){
+      initShow(){//初始化下拉菜单为不显示
         this.show.isField=false;
         this.show.isDepartment=false;
         this.show.isMode=false;
         this.show.isObject=false;
       },
-      toggle(n){
+      toggle(n){//点击下拉根据index切换显示
         switch (n){
           case 0:{
             this.show.isField=!this.show.isField;
@@ -123,10 +124,19 @@
             this.show.isMode=false;
           }break;
         }
-        console.log(this.show)
       },
-      search(type,item){
+      clearCondition(){//清空检索条件①设置搜索数据为空
+        this.searchData.type2='';
+        this.searchData.managerDeptId='';
+        this.searchData.lineAble='';
+        this.searchData.userGroupId='';
+        this.condition.splice(0,this.condition.length);//清空数组
+        this.isCondition=false;//设置检索条件框不显示
+        this.getList();//请求列表渲染
+      },
+      search(type,item){//选择下拉列表一项搜索
         this.initShow();
+        this.isCondition=true;
         if(type=="field"){
           this.condition[0]='服务领域：'+item.name;
           this.searchData.type2=item.id;
@@ -143,12 +153,13 @@
           this.condition[3]='服务对象：'+item.groupName;
           this.searchData.userGroupId=item.id;
         }
+        this.getList();
       },
-      getList(){
+      getList(){//拉取列表
         this.$ajax.post('/api/service_portal/list',this.searchData)
             .then((res)=>{
               console.log(res);
-              this.searchList=res.page;
+              this.searchList=res.data.page.rows;
             })
             .catch((error)=>{
               console.log(error);
@@ -195,6 +206,7 @@
         top: 155/@base;
         z-index: 99;
         width: 100%;
+        background: #fff;
         box-shadow: 0 6px 12px rgba(0, 0, 0, .175);
     }
     ul.menu li{
@@ -219,28 +231,35 @@
     ul.list{
         padding: 0 10/@base;
     }
-    ul.list li{
+    ul.list a{
         display: flex;
         margin: 5/@base 0;
         padding: 15/@base;
         border: 1px solid #bfbfbf;
     }
-    ul.list li img{
+    ul.list a img{
         width: 50/@base;
         height: 50/@base;
         margin-right: 15/@base;
     }
-    ul.list li .info{
+    ul.list a .info{
         display: flex;
         flex-flow: column;
         justify-content: space-between;
     }
-    ul.list li .info header{
+    ul.list a .info header{
         font-size: 14/@base;
     }
-    ul.list li .info p{
+    ul.list a .info p{
         font-size: 12/@base;
         color: #959595;
         margin: 0;
+    }
+    ul.list a .info p i:first-child{
+        color: #e60012;
+        margin-left: 10/@base;
+    }
+    ul.list a .info p i:last-child{
+        margin-left: 10/@base;
     }
 </style>
